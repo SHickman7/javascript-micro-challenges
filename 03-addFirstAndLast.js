e3fe1a2d99ca1a1cbfbf1f19477feb1bbd1968ce
Stephanie Hickman <stephick@gmail.com>
2024-01-17 16:36:15 -0600

    completed question 03-addFirstAndLast

--- 03-addFirstAndLast.js
+++ 03-addFirstAndLast.js
@@ -10,10 +10,24 @@
 // addFirstAndLast([-9, 12, 33, 29])
 //     should evaluate to 20
 
+//My plan:
+  //create a variable that taken in an array of numbers
+  //the function should loop through the array and list the values of the array
+  //the function should RETURN the sum of this first and last value
 
-function addFirstAndLast() {
 
+let firstArray = [ 3, 2, 6];
+let secondArray = [-9, 12, 33, 29]
+
+function addFirstAndLast(array) {
+  for (let number of array){
+     console.log(number);
+  }
+return array[0] + array[array.length-1]
 }
+
+console.log('Expecting a sum of 9:', addFirstAndLast(firstArray));
+console.log('Expecting a sum of 20:', addFirstAndLast(secondArray));
 
 try {
   module.exports = addFirstAndLast;
