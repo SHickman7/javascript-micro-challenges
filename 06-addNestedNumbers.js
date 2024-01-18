eec1bea4a273053f38979925ba5aeaa5d8c32f8d
Stephanie Hickman <stephick@gmail.com>
2024-01-18 07:07:48 -0600

    progress on 06-addNestedNumbers

--- 06-addNestedNumbers.js
+++ 06-addNestedNumbers.js
@@ -9,19 +9,36 @@
 // arrays and return the value.
 
 // Examples:
-// let nestedNumbers = [
-//   [1, 5],
-//   [2],
-//   [5, 10, 3]
-// ]
+let nestedNumbers = [
+  [1, 5],
+  [2],
+  [5, 10, 3]
+]
+
 
 // addNestedNumbers()
 //     should evaluate to 26
 
 
-function addNestedNumbers() {
+function addNestedNumbers(array) {
+  let sum = 0;
 
+  for (let i = 0; i < array.length; i++){
+    console.log (array [i]);
+    for (let j = 0; j < array[i].length; j++){
+      console.log( array[i][j])
+    
+ 
+    
+    
+    }
+   
+  }
+  
 }
+
+console.log(addNestedNumbers(nestedNumbers));
+
 
 try {
   module.exports = addNestedNumbers;
